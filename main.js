23396424e8b20205415ed9f0fb43b0f7623eaaf6
Dmateo321 <131504034+Dmateo321@users.noreply.github.com>
2023-05-06 13:06:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-console.log("main.js");
+// prompt ("hola soy Daniel");
+// // path: main.js
+// alert ("hola soy ana no voy a clases");
 
+// var numero1 = prompt ("introduce un numero")
+// var numero2 = prompt ("introduce un numero")
 
-let a = 2
-let b = 3
+// var resultado = parseInt(numero1) + parseInt(numero2)
+// alert("el resultado es" + resultado);
 
+// let numero1 = 5;
+// let numero2 = 10;
 
-let suma = a + b
+// let resultado = numero1 + numero2;
+// console.log("resultados: " , resultado);
 
-console.log(suma);
+const form = document.querySelector("form");
+const input = document.getElementById("btn");
+
+input.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(form.elements[0].value);
+    console.log(form.elements[1].value);
+
+    const nombre = form.elements[0].value;
+    const contrasena = form.elements[1].value;
+
+    if(nombre === "admin" && contrasena === "1234"){
+        return window.open("https://dmateo321.github.io/Clase-29/");
+    }else{
+        return alert("datos incorrectos");
+    }
+});
+
+console.log({form, input});
+
